012831fa1335ee334086ee430cdc6231d267c74d
NataliiaBreus <breusnataly@gmail.com>
2021-05-06 10:54:30 +0300

    refactor create element

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,77 +1,38 @@
 'use strict';
-const cardContainer = document.getElementById('root');
-const cardElements = data.map((place)=>{
-    return createPlaceCards(place)
-});
-console.log(cardElements);
+/**
+ * 
+ * @param {string} tagName
+ * @param {object} options
+ * @param {string[]} options.classNames - css classes
+ * @param {object} options.handlers - event handlers
+ * @param {object} options.attributes - attributes
+ * @param {...Node} children
+ * @returns {HTMLElement}
+ */
+function createElement (
+  tagName,
+  {classNames = [], handlers = {}, attributes = {} } = {},
+  ...children
+) {
+  const elem = document.createElement(tagName);
+  elem.classList.add(...classNames);
+/*
+attributes object example
+  const attrs = {
+    src: "https://example.com",
+    value: "text",
+    name: "textInput",
+  };
 
-cardContainer.append(...cardElements);
-
-function createPlaceCards(place) {
-    const card = document.createElement('li');
-    card.classList.add('cardWrapper');
-
-    const container = document.createElement('article');
-    container.classList.add('cardContainer');
-
-    const imageWrapper = document.createElement("div");
-    imageWrapper.classList.add("cardImageWrapper");
-    imageWrapper.style.backgroundColor = stringToColor(place.name || "");
-
-    const initials = document.createElement('div');
-    initials.classList.add('initials');
-    //initials.innerText = place.name[0];
-    initials.append(document.createTextNode(place.name[0] || ""));
-
-    const img = document.createElement('img');
-    img.classList.add('cardImage');
-    img.src = place.profilePicture;
-
-    img.addEventListener('error', handleImageError);
-    img.addEventListener('load', handleImageLoad);
-
-    imageWrapper.append(initials, img);
-
-    const contentWrapper = document.createElement("div");
-    contentWrapper.classList.add("contentWrapper");
-
-    const name = document.createElement('h3');
-    name.classList.add("cardName");
-    name.append(document.createTextNode(place.name || ""));
-
-    const description = document.createElement("p");
-    description.classList.add("cardDescription");
-
-    contentWrapper.append(name, description);
-    container.append(imageWrapper, contentWrapper);
-    card.append(container);
-
-   return card; 
+*/
+for(const[attrName, attrValue] of Object.defineProperties(attributes)) {
+  elem.setAttribute(attrName, attrValue);
 }
 
-/*
-EVENT HANDLERS
-*/
- function handleImageError({target}) {
-     target.remove();
- }
-
- function handleImageLoad({target}){
-     target.hidden = false;
- }
- /*
- UTILS
- */
-
- function stringToColor(str) {
-    var hash = 0;
-    for (var i = 0; i < str.length; i++) {
-      hash = str.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    var colour = '#';
-    for (var i = 0; i < 3; i++) {
-      var value = (hash >> (i * 8)) & 0xFF;
-      colour += ('00' + value.toString(16)).substr(-2);
-    }
-    return colour;
+  for (const [eventType, eventHandler] of Object.entries(handlers)) {
+    elem.addEventListener (eventType, eventHandler);
   }
+
+  elem.append(...children);
+  return elem;
+}
